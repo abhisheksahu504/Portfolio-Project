a0800ecc276e9c12d70406e38164b3868bd66323
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-08-10 12:32:41 +0530

    adding animation using framer motion

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,24 @@
-import logo from "../assets/kevinRushLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa6";
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <img className="mx-2 w-10" src={logo} alt="logo" />
+        {/* <img className=" bg-black mx-2 w-10" src={logo} alt="logo" /> */}
+        <FaLaptopCode className="text-4xl" />
       </div>
       <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin />
-        <FaGithub />
-        <FaInstagram />
-        <FaSquareXTwitter />
+        <a href="https://www.linkedin.com/in/iamabhisheksahu/">
+          <FaLinkedin />
+        </a>
+        <a href="https://github.com/abhisheksahu504/">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/abhishek.is.the.name/">
+          <FaInstagram />
+        </a>
       </div>
     </nav>
   );
