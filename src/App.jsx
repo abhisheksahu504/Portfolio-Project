df2284d14fd0ecf244281fdfbc95209a0a076d32
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-08-20 16:29:56 +0530

    removed about me section which might be unnecessary

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
-import About from "./components/About";
+// import About from "./components/About";
 import Technologies from "./components/Technologies";
 import Experience from "./components/Experience";
 import Projects from "./components/Projects";
@@ -18,7 +18,7 @@ const App = () => {
       <div className="container mx-auto px-8">
         <Navbar />
         <Hero />
-        <About />
+        {/* <About /> */}
         <Technologies />
         <Experience />
         <Education />
